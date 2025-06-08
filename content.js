48c0cc5a6920217178afa0cccbbc100466cb28f0
Egor <k.egorray01@gmail.com>
2025-06-08 10:31:41 +0300

    Панельная версия киоска, где кнопки содержатся в панели, слева от страницы

--- content.js
+++ content.js
@@ -1,39 +1,69 @@
 // Получаем данные из хранилища
-chrome.storage.sync.get(['enablebtn','enabletimer','link','buttontext','timer','link'], function(result) {
-    if(result.enablebtn){
-
-        createFloatingLink(result.link || 'https://example.com', result.buttontext || 'Кнопка');
-        
-        function createFloatingLink(url, txt) {
-            // Создаем div для ссылки
+chrome.storage.sync.get(['enablebutton0','enablebutton1','enablebutton2','enabletimer','link','buttontext1','buttontext2','timer','panelwidth'], function(result) {
+    if(result.enablebutton0){
+        // Создаем div для ссылки
+            const panelwidth = result.panelwidth;
             const linkContainer = document.createElement('div');
-            linkContainer.id = 'floating-link';
+            linkContainer.id = 'button-link';
+            linkContainer.style.width = panelwidth;
+        // создание домашней кнопки
+        if(result.enablebutton1){
+            createHomeButton(result.link || 'https://example.com', result.buttontext1 || "Главное меню");
+
+            function createHomeButton(url, txt) {
+                // Создаем ссылку
+                const frame = document.createElement('div');
+                const link = document.createElement('a');
+                frame.id = 'button-frame';
+                link.id = 'home-button-id';
+                link.href = url;
+                link.textContent = txt;
+                
+                frame.appendChild(link);
+                linkContainer.appendChild(frame);
+                document.body.appendChild(linkContainer);
+    
+                // Сдвигаем содержимое страницы под панель упавления
+                document.body.style.marginLeft = panelwidth + 'px';
+            }
+        }
+        // создание кнопки "назад"
+        if(result.enablebutton2){
+            createBackButton(result.buttontext2 || 'Назад');
             
-            // Создаем ссылку
-            const link = document.createElement('a');
-            link.href = url;
-            link.textContent = txt;
-            
-            linkContainer.appendChild(link);
-            document.body.appendChild(linkContainer);
+            function createBackButton(txt) {
+                // Создаем ссылку
+                const frame = document.createElement('div');
+                const link = document.createElement('a');
+                frame.id = 'button-frame';
+                link.id = 'back-button-id';
+                link.href.onclick = () => window.history.back();
+                link.textContent = txt;
+                
+                frame.appendChild(link);
+                linkContainer.appendChild(frame);
+                document.body.appendChild(linkContainer);
+    
+                // Сдвигаем содержимое страницы под панель упавления
+                document.body.style.marginLeft = panelwidth + 'px';
+            }
+        }
+    
+        // условие, если включен таймер
+        if(result.enabletimer & window.location.href != result.link){
+                const redirectUrl = result.link;
+                let idleTimer;
+                
+                const resetIdleTimer = () => {
+                  // отменяем предыдущий таймер
+                  clearTimeout(idleTimer);
+                  // запускаем отсчёт на 5 секунд
+                  idleTimer = setTimeout(() => window.location.href = redirectUrl, result.timer*1000);
+                };
+    
+                // регистрируем события движения мыши и нажатия клавиш
+                ['mousemove', 'keypress'].forEach(event => document.addEventListener(event, resetIdleTimer));
+                resetIdleTimer();
         }
     }
-    // условие, если включен таймер
-    if(result.enabletimer & window.location.href != result.link){
-            const redirectUrl = result.link;
-            let idleTimer;
-            
-            const resetIdleTimer = () => {
-              // отменяем предыдущий таймер
-              clearTimeout(idleTimer);
-              // запускаем отсчёт на 5 секунд
-              idleTimer = setTimeout(() => window.location.href = redirectUrl, result.timer*1000);
-            };
-
-            // регистрируем события движения мыши и нажатия клавиш
-            ['mousemove', 'keypress'].forEach(event => document.addEventListener(event, resetIdleTimer));
-            resetIdleTimer();
-    }
 });
-
-
--- options.js
+++ options.js
@@ -1,22 +1,39 @@
 // Сохранение изменений
 document.getElementById('save').addEventListener('click', function() {
     const link = document.getElementById('link').value;
-    const buttontext = document.getElementById('buttontext').value;
-    const enablebtn = document.getElementById('enablebutton').checked;
+    const buttontext1 = document.getElementById('buttontext1').value;
+    const buttontext2 = document.getElementById('buttontext2').value;
+    const enablebutton0 = document.getElementById('enablebutton0').checked;
+    const enablebutton1 = document.getElementById('enablebutton1').checked;
+    const enablebutton2 = document.getElementById('enablebutton2').checked;
     const timer = document.getElementById('timer').value;
     const enabletimer = document.getElementById('enabletimer').checked;
-    chrome.storage.sync.set({ link: link, buttontext: buttontext, enablebtn: enablebtn, timer: timer, enabletimer: enabletimer}, function() {
+    const panelwidth = document.getElementById('panelwidth').value;
+    const panelwidthvalue = document.getElementById('panelwidthvalue').textContent;
+    chrome.storage.sync.set({ link: link, buttontext1: buttontext1, buttontext2: buttontext2,
+         enablebutton0: enablebutton0, enablebutton1: enablebutton1, enablebutton2: enablebutton2,
+         timer: timer, enabletimer: enabletimer, panelwidth: panelwidth, panelwidthvalue:panelwidthvalue}, function() {
         alert('Изменения сохранены!');
     });
 });
 
 
 // Загрузка сохраненных изменений при открытии страницы
-chrome.storage.sync.get(['link', 'buttontext','enablebtn','timer','enabletimer'], function(result) {
+chrome.storage.sync.get(['link','buttontext1','buttontext2','enablebutton0','enablebutton1','enablebutton2','timer','enabletimer', 'panelwidth','panelwidthvalue'], function(result) {
     document.getElementById('link').value = result.link || '';
-    document.getElementById('buttontext').value = result.buttontext || '';
-    document.getElementById('enablebutton').checked = result.enablebtn || '';
+    document.getElementById('buttontext1').value = result.buttontext1 || '';
+    document.getElementById('buttontext2').value = result.buttontext2 || '';
+    document.getElementById('enablebutton0').checked = result.enablebutton0 || '';
+    document.getElementById('enablebutton1').checked = result.enablebutton1 || '';
+    document.getElementById('enablebutton2').checked = result.enablebutton2 || '';
     document.getElementById('timer').value = result.timer || '';
     document.getElementById('enabletimer').checked = result.enabletimer || '';
+    document.getElementById('panelwidth').value = result.panelwidth || '';
+    document.getElementById('panelwidthvalue').textContent = result.panelwidth || '';
 });
 
+// Обновление значения ширины панели навигации
+const panelwidth = document.getElementById('panelwidth');
+panelwidth.addEventListener('input', function(){
+    document.getElementById('panelwidthvalue').textContent = panelwidth.value;
+})
